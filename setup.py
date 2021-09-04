05f9be46941fd185e1dfbea5d1ed229fcfe746a9
Ruhong <seahrh@gmail.com>
2021-09-04 12:51:01 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0"
+setup(
+    name="nlp-examples",
+    version=__version__,
+    python_requires="~=3.7",
+    install_requires=["nltk==3.6.2", "spacy==3.1.2", "tqdm==4.62.2"],
+    extras_require={
+        "tests": [
+            "black==20.8b1",
+            "mypy==0.812",
+            "pytest==6.2.3",
+            "pytest-cov==2.11.1",
+        ],
+        "notebook": ["jupyterlab==1.2.16", "seaborn==0.11.1", "ipywidgets==7.6.3"],
+    },
+    packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    description="NLP code examples",
+    license="MIT",
+    author="seahrh",
+    author_email="seahrh@gmail.com",
+    url="https://github.com/seahrh/nlp-examples",
+)
